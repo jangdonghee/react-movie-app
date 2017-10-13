7fad49fcd97e793e5b9d711217da27a5369f6672
jangdonghee <lbghini@mgail.com>
2017-10-13 22:01:32 +0900

    search 추가

--- src/App.js
+++ src/App.js
@@ -1,31 +1,66 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
+import Search from './Search';
 
 class App extends Component {
-  state = {}
+  constructor(props){
+    super(props);
+
+    this.state = {
+      searchTitle: "",
+      searchType: "quality",
+      searchValue: "All"
+    }
+    
+    this._searchTitle = this._searchTitle.bind(this);
+    this._searchType = this._searchType.bind(this);
+  }
 
   componentDidMount(){
+    console.log("componentDidMount")
     this._getMovies();
+  }
+
+  componentWillReceiveProps(nextProps){
+    // console.log("nextProps.searchValue : ", nextProps.searchValue);
+    // this.setState({
+    //   searchType: nextProps.searchType,
+    //   searchValue: nextProps.searchValue
+    // });
+    // this._getMovies();
   }
 
   _getMovies = async () => {
     const movies = await this._callApi();
-    console.log(movies);
+    //console.log("movies ::: " + movies)
     this.setState({
       movies: movies
     });
+    console.log("searchType // ::: " + this.state.searchType);
+    console.log("searchValue ::: " + this.state.searchValue);
   }
 
   _callApi = () => {
-    return fetch('https://yts.ag/api/v2/list_movies.json?sort_by=down_count')
+    console.log('url ::: https://yts.ag/api/v2/list_movies.json?sort_by=down_count&'+this.state.searchType+"="+this.state.searchValue);
+    return fetch('https://yts.ag/api/v2/list_movies.json?sort_by=down_count&'+this.state.searchType+"="+this.state.searchValue)
     .then(response => response.json())
     .then(json => json.data.movies)
     .catch((err) => {console.log("err")})
   }
 
-  _renderMovies = () => {
-    const movieList = this.state.movies.map((movie, index) => {
+  _mapToMovieList = (movieList) => {
+    movieList = movieList.filter(
+      (movie) => {
+        if(this.state.searchTitle != ""){
+          return movie.title_english.indexOf(this.state.searchTitle) > -1;
+        }else{
+          return this.state.movies;
+        }
+      }
+    );
+
+    return movieList.map((movie, index) => {
       return <Movie 
         title={movie.title_english} 
         poster={movie.medium_cover_image} 
@@ -33,17 +68,34 @@ class App extends Component {
         synopsis={movie.synopsis}
         key={movie.id} />
     });
-
-    return movieList;
   }
 
+  _searchTitle = (title) => {
+    this.setState({
+      searchTitle: title
+    });
+  }
+
+  _searchType = (searchType, searchValue) => {
+    this.setState({
+      searchType: searchType,
+      searchValue: searchValue
+    },function(){
+      this._getMovies();
+    }
+  );
+  };
+
   render() {
-    const {movies} = this.state;
     return (
-      <div className={movies ? "App" : "App--loading"}>
+      <div className="container">
+        <Search onSearchTitle={this._searchTitle} onSearchType={this._searchType}/>
+        <div className={this.state.movies ? "App" : "App--loading"}>
         {
-          this.state.movies ? this._renderMovies() : "Loading.."
+          //this.state.movies ? this._renderMovies() : "Loading.."
+          this.state.movies ? this._mapToMovieList(this.state.movies) : "Loading.."
         }
+        </div>
       </div>
     );
   }
---
+++ src/Search.js
@@ -0,0 +1,128 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import './Search.css';
+
+class Search extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            title: "",
+            searchType: "",
+            searchValue: ""
+        }
+
+        this._searchTitle = this._searchTitle.bind(this);
+        this._searchType = this._searchType.bind(this);
+    }
+
+    _searchTitle = (e) => {
+        this.setState(
+            {
+                title: e.target.value
+            }, function (){
+                this.props.onSearchTitle(this.state.title);
+            }
+        )
+    }
+
+    _searchType = (e) => {
+        this.setState(
+            {
+                searchType: e.target.name,
+                searchValue: e.target.value
+            }, function (){
+                this.props.onSearchType(this.state.searchType, this.state.searchValue)
+            }
+        );
+    }
+
+    render(){
+        return(
+            <div className="Search__Wrap">
+                <div className="Search__Box">
+                    <input type="text" placeholder="Movie Search" onChange={this._searchTitle} />
+                </div>
+                <div className="Search__Type">
+                    <div className="Search__Type__Item">
+                        <p>Quality</p>
+                        <select name="quality" onChange={this._searchType}>
+                            <option value="all">All</option>
+                            <option value="720p">720p</option>
+                            <option value="1080p">1080p</option>
+                            <option value="3D">3D</option>
+                        </select>
+                    </div>
+                    <div>
+                        <p>Genre</p>
+                        <select name="genre">
+                            <option value="all">All</option>
+                            <option value="action">Action</option>
+                            <option value="adventure">Adventure</option>
+                            <option value="animation">Animation</option>
+                            <option value="biography">Biography</option>
+                            <option value="comedy">Comedy</option>
+                            <option value="crime">Crime</option>
+                            <option value="documentary">Documentary</option>
+                            <option value="drama">Drama</option>
+                            <option value="family">Family</option>
+                            <option value="fantasy">Fantasy</option>
+                            <option value="film-noir">Film-Noir</option>
+                            <option value="game-show">Game-Show</option>
+                            <option value="history">History</option>
+                            <option value="horror">Horror</option>
+                            <option value="music">Music</option>
+                            <option value="musical">Musical</option>
+                            <option value="mystery">Mystery</option>
+                            <option value="news">News</option>
+                            <option value="reality-tv">Reality-TV</option>
+                            <option value="romance">Romance</option>
+                            <option value="sci-fi">Sci-Fi</option>
+                            <option value="sport">Sport</option>
+                            <option value="talk-show">Talk-Show</option>
+                            <option value="thriller">Thriller</option>
+                            <option value="war">War</option>
+                            <option value="western">Western</option>
+                        </select>
+                    </div>
+                    <div>
+                        <p>Genre</p>
+                        <select name="rating">
+                            <option value="0">All</option>
+                            <option value="9">9+</option>
+                            <option value="8">8+</option>
+                            <option value="7">7+</option>
+                            <option value="6">6+</option>
+                            <option value="5">5+</option>
+                            <option value="4">4+</option>
+                            <option value="3">3+</option>
+                            <option value="2">2+</option>
+                            <option value="1">1+</option>
+                        </select>
+                    </div>
+                    <div>
+                        <p>Order By</p>
+                        <select name="order_by">
+                            <option value="latest">Latest</option>
+                            <option value="oldest">Oldest</option>
+                            <option value="seeds">Seeds</option>
+                            <option value="peers">Peers</option>
+                            <option value="year">Year</option>
+                            <option value="rating">Rating</option>
+                            <option value="likes">Likes</option>
+                            <option value="alphabetical">Alphabetical</option>
+                            <option value="downloads">Downloads</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+ Search.defaultProp = {
+    title: "",
+    searchType: "",
+    searchValue: ""
+}
+
+export default Search;
